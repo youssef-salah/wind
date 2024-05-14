c96e758b538c3210a6aba55a9f629a6db193630d
youssef-salah <77450576+youssef-salah@users.noreply.github.com>
2024-05-14 19:21:28 +0300

    Update wind_pred.py

--- wind_pred.py
+++ wind_pred.py
@@ -2,10 +2,10 @@ from flask import Flask, request, jsonify, make_response
 import joblib
 import pandas as pd
 
-app = Flask(__name__)
+wind_pred = Flask(__name__)
 regr_energy = joblib.load('wind_prediction.joblib')
 
-@app.route('/wind', methods=['POST', 'GET'])
+@wind_pred.route('/wind', methods=['POST', 'GET'])
 def handle_data():
    if request.method == 'POST':
       jdata = request.json
@@ -19,28 +19,28 @@ def handle_data():
 
 
 # Create DataFrame with the received data
-   input_data = {
-   "wind_speed_m_s": wind_speed,
-   "pressure_atm": pressure,
-   "wind_direction_deg": wind_direction,
-   "air_temperature_c": air_temperature,
-   "Month": month
-   }
-   input_df = pd.DataFrame([input_data])
+      input_data = {
+      "wind_speed_m_s": wind_speed,
+      "pressure_atm": pressure,
+      "wind_direction_deg": wind_direction,
+      "air_temperature_c": air_temperature,
+      "Month": month
+      }
+      input_df = pd.DataFrame([input_data])
 
-   predicted_wind_energy = regr_energy.predict(input_df)
-   predicted_wind_energy = predicted_wind_energy.tolist()
+      predicted_wind_energy = regr_energy.predict(input_df)
+      predicted_wind_energy = predicted_wind_energy.tolist()
 
 # Create response with CORS headers
-   response = make_response(jsonify({'predicted_wind_energy': predicted_wind_energy[0]}))
-   response.headers['Access-Control-Allow-Origin'] = '*'
-   response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-   response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+      response = make_response(jsonify({'predicted_wind_energy': predicted_wind_energy[0]}))
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
 
    return response
 
 if __name__ == '__main__':
- app.run(debug=True, host='0.0.0.0', port=3000)
+ wind_pred.run(debug=True, host='0.0.0.0', port=3000)
 
 
 
