a44564146be03b041477973e78c751af3ea3d406
youssef-salah <77450576+youssef-salah@users.noreply.github.com>
2024-05-16 01:04:50 +0300

    Update wind_pred.py

--- wind_pred.py
+++ wind_pred.py
@@ -15,7 +15,7 @@ def handle_data():
          
       elif request.method == 'GET':
    # Get parameters from query string
-         params = ["wind_speed_m_s","pressure_atm","wind_direction_deg","air_temperature_c","Month"]
+         params = ("wind_speed_m_s","pressure_atm","wind_direction_deg","air_temperature_c","Month")
          jdata = {params : float (request.args.get(params , 0)) for param in params}
          jdata_df = pd.DataFrame([jdata])
 # Create DataFrame with the received data
