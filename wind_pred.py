17e6a4de5ac07cd0bc30347d5371bf468e4c79ac
youssef-salah <77450576+youssef-salah@users.noreply.github.com>
2024-05-16 00:42:31 +0300

    Update wind_pred.py

--- wind_pred.py
+++ wind_pred.py
@@ -7,38 +7,34 @@ regr_energy = joblib.load('wind_prediction.joblib')
 
 @app.route('/wind', methods=['POST', 'GET'])
 def handle_data():
-   response = None   
-   if request.method == 'POST':
-      jdata = request.json
-   elif request.method == 'GET':
-# Get parameters from query string
-      wind_speed = float(request.args.get('wind_speed_m_s'))
-      wind_direction = float(request.args.get('wind_direction_deg'))
-      pressure = float(request.args.get('pressure_atm'))
-      air_temperature = float(request.args.get('air_temperature_c'))
-      month= int(request.args.get('Month'))
+   try :
 
-
+      if request.method == 'POST':
+         jdata = request.json
+         jdata_df = pd.DataFrame([jdata])
+         
+      elif request.method == 'GET':
+   # Get parameters from query string
+         params = ["wind_speed_m_s","pressure_atm","wind_direction_deg","air_temperature_c","Month"]
+         jdata = {params : float (request.args.get(params , 0)) for param in params}
+         jdata_df = pd.DataFrame([jdata])
 # Create DataFrame with the received data
-   input_data = {
-   "wind_speed_m_s": wind_speed,
-   "pressure_atm": pressure,
-   "wind_direction_deg": wind_direction,
-   "air_temperature_c": air_temperature,
-   "Month": month
-   }
-   input_df = pd.DataFrame([input_data])
-
-   predicted_wind_energy = regr_energy.predict(input_df)
-   predicted_wind_energy = predicted_wind_energy.tolist()
+      
+      predicted_wind_energy = regr_energy.predict(jdata_df)
+      predicted_wind_energy = predicted_wind_energy.tolist()
 
 # Create response with CORS headers
-   response = make_response(jsonify({'predicted_wind_energy': predicted_wind_energy[0]}))
-   response.headers['Access-Control-Allow-Origin'] = '*'
-   response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-   response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+      response = make_response(jsonify({'predicted_wind_energy': predicted_wind_energy[0]}))
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
 
-   return response
+      return response
+
+   except Exception as e :
+      response = make_response(jsonify({'error' : str(e)}), 400)
+
+      return response
 
 if __name__ == '__main__':
  app.run(debug=True, host='0.0.0.0', port=3000)
